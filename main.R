c078b28adac3e758de6b655ecf1b0a48bb307b5a
Aswxu <xrz110302@gmail.com>
2016-05-23 21:32:46 +1000

    23/05/16 Trying to make gp stuffs function
    GP stuffs still on going

--- main.R
+++ main.R
@@ -1,5 +1,11 @@
 #G3tYourMa5t3r!!! Yaarctopus
 
+#uncomment to install packages
+#install.packages("igraph", dependencies = TRUE)
+#install.packages("lpbrim", dependencies = TRUE)
+#install.packages("bipartite", dependencies = TRUE)
+#install.packages("Matrix", dependencies = TRUE)
+
 #library statements
 require(igraph)
 require(lpbrim)
@@ -79,8 +85,8 @@ brim.mut.info<-function(link.data,ind.vec,n.net,s.mat.gp.true){
   norm.mut.info.brim.vec
 }
 
-#function #4 using the optimal? algorithm on the projection of the bipartite graph to get the result
-proj.mut.info<-function(link.data,ind.vec,n.net,s.mat.gp.true){
+#function #4 using the cluster_louvain algorithm on the projection of the bipartite graph to get the result
+proj.mut.info.louvain<-function(link.data,ind.vec,n.net,s.mat.gp.true){
   norm.mut.info.proj.vec<-NULL
   ngp=sum(ind.vec)
   for(i in 1:n.net){
@@ -91,14 +97,34 @@ proj.mut.info<-function(link.data,ind.vec,n.net,s.mat.gp.true){
     V(temp.bipa.graph)$name<-c(1:(npa+ngp))
     V(temp.bipa.graph)$type<-ind.vec==0
     temp.proj.graph<-bipartite_projection(temp.bipa.graph)$proj1
-    wc<-membership(cluster_optimal(temp.proj.graph))
+    lv<-membership(cluster_louvain(temp.proj.graph))
+    s.mat.proj.gp<-matrix(0,ncol=max(lv),nrow=ngp)
+    for(j in 1:ngp)s.mat.proj.gp[j,lv[j]]<-1
+    norm.mut.info.proj.vec<-c(norm.mut.info.proj.vec,norm.mut.infor(s.mat.gp.true,s.mat.proj.gp))
+  }
+  norm.mut.info.proj.vec
+}
+
+
+#function #5 using the cluster_walktrap algorithm on the projection of the bipartite graph to get the result
+proj.mut.info.walktrap<-function(link.data,ind.vec,n.net,s.mat.gp.true){
+  norm.mut.info.proj.vec<-NULL
+  ngp=sum(ind.vec)
+  for(i in 1:n.net){
+    #make adjacency matrix
+    link.data.adj<-rbind(cbind(matrix(rep(0,ngp^2),ncol=ngp),link.data[[i]]),cbind(t(link.data[[i]]),matrix(rep(0,npa^2),ncol=npa)))
+    #produce the bipartite graph object
+    temp.bipa.graph<-graph_from_adjacency_matrix(link.data.adj)
+    V(temp.bipa.graph)$name<-c(1:(npa+ngp))
+    V(temp.bipa.graph)$type<-ind.vec==0
+    temp.proj.graph<-bipartite_projection(temp.bipa.graph)$proj1
+    wc<-membership(cluster_walktrap(temp.proj.graph,steps=5))
     s.mat.proj.gp<-matrix(0,ncol=max(wc),nrow=ngp)
     for(j in 1:ngp)s.mat.proj.gp[j,wc[j]]<-1
     norm.mut.info.proj.vec<-c(norm.mut.info.proj.vec,norm.mut.infor(s.mat.gp.true,s.mat.proj.gp))
   }
   norm.mut.info.proj.vec
 }
-
 
 #Part 1 #for a set p.in/p.out
 #control parameters
@@ -114,9 +140,9 @@ ncomm.true<-5
 p.in=0.6
 p.out=0.3
 #numbers of networks generated
-n.net=1000
+n.net=10000
 #debugging setting
-#n.net=3
+#n.net=1000
 
 #simulation before apply algorithms
 #specify the #TRUE# index vector&matrix
@@ -136,8 +162,12 @@ link.data<-link.create(ind.vec,comm.ind.true,p.in,p.out,n.net)
 norm.mut.info.brim.vec<-brim.mut.info(link.data,ind.vec,n.net,s.mat.gp.true)
 
 
-#Projected Unipartite-optimal?
-norm.mut.info.proj.vec<-proj.mut.info(link.data,ind.vec,n.net,s.mat.gp.true)
+#Projected Unipartite-louvain
+norm.mut.info.louvain.vec<-proj.mut.info.louvain(link.data,ind.vec,n.net,s.mat.gp.true)
+
+#Projected Unipartite-walktrap
+norm.mut.info.walktrap.vec<-proj.mut.info.walktrap(link.data,ind.vec,n.net,s.mat.gp.true)
+
 
 
 #Part 2 #for different set p.in/p.out
@@ -194,7 +224,7 @@ crazy.function<-function(p.in.lower,p.in.upper,step,p.out.lower,n.net,ngp,npa,nc
       
       #Bipartite-BRIM
       norm.mut.info.brim.vec<-brim.mut.info(link.data,ind.vec,n.net,s.mat.gp.true)
-            
+      
       #Projected Unipartite-optimal?
       norm.mut.info.proj.vec<-proj.mut.info(link.data,ind.vec,n.net,s.mat.gp.true)
       
@@ -218,9 +248,22 @@ cross.fingers<-crazy.function(p.in.lower,p.in.upper,step,p.out.lower,n.net,ngp,n
 
 
 
-
-
-
+#part3 ploting stuffs
+#projection
+adj.mat<-matrix(c(rep(0,3),1,rep(0,3),1,rep(0,3),1,rep(1,3),0),nrow=4,ncol=4)
+temp.bipa.graph<-graph_from_adjacency_matrix(adj.mat,mode="undirected")
+V(temp.bipa.graph)$name<-c(1:3,"A")
+V(temp.bipa.graph)$type<-c(rep(1,3),0)
+lay.bi<-layout.bipartite(temp.bipa.graph)
+plot(temp.bipa.graph, layout = lay.bi[, c(1,2)],vertex.size=30,vertex.color="darkblue",vertex.label.color="yellow",vertex.label.font=2,edge.color="black")
+temp.proj.graph<-bipartite_projection(temp.bipa.graph,multiplicity = TRUE,which="TRUE")
+plot(temp.proj.graph,vertex.size=30,vertex.color="darkblue",vertex.label.color="yellow",vertex.label.font=2,edge.color="black")
+adj.mat<-matrix(c(rep(0,3),1,1,0,rep(0,3),1,0,1,rep(0,3),0,1,1,1,1,0,rep(0,3),1,0,1,rep(0,3),0,1,1,rep(0,3)),nrow=6,ncol=6)
+temp.bipa.graph<-graph_from_adjacency_matrix(adj.mat,mode="undirected")
+V(temp.bipa.graph)$name<-c(1:3,"A","B","C")
+V(temp.bipa.graph)$type<-c(rep(1,3),0,0,0)
+lay.bi<-layout.bipartite(temp.bipa.graph)
+plot(temp.bipa.graph, layout = lay.bi[, c(1,2)],vertex.size=30,vertex.color="darkblue",vertex.label.color="yellow",vertex.label.font=2,edge.color="black")
 
 
 
@@ -234,12 +277,62 @@ cross.fingers<-crazy.function(p.in.lower,p.in.upper,step,p.out.lower,n.net,ngp,n
 
 #real GP-patients dataset
 #the scrambled_ppn have a initial shift of 99(1->100), therefore create a new raw.csv dataset file.
-#Convert the raw data into a huge matrix and store it as "Medicare_link_data"
-#raw<-read.csv("raw.csv",header=TRUE)
-#medicare.link.data<-Matrix(0,nrow=max(raw$scrambled_prov),ncol=max(raw$scrambled_ppn))
-#for(i in 1:nrow(raw)){
-#  temp<-raw[i,]
-#  medicare.link.data[temp$scrambled_prov,temp$scrambled_ppn]<-medicare.link.data[temp$scrambled_prov,temp$scrambled_ppn]+1
-#}
-#writeMM(medcare.link.data,file="Medicare_link_data")
-medicare.link.data<-readMM("Medicare_link_data")
+raw<-read.csv("raw.csv",header=TRUE)
+#max(table(raw$scrambled_pcode))
+##[1] 11692
+#c(1000:1541)[table(raw$scrambled_pcode)==11692]
+##[1] 1446
+#mean(table(raw$scrambled_pcode))
+##[1] 938.9207
+#c(1000:1541)[table(raw$scrambled_pcode)==938]
+##[1] 1153
+
+#function #6 for rearrange the number
+reflect<-function(ref,data){
+  neo.ref<-c(1:length(ref))
+  neo<-rep(0,length(data))
+  for(i in 1:length(ref)){
+    temp<-data==ref[i]
+    neo[temp]<-neo.ref[i]
+  }
+  neo
+}
+
+
+#pick pcode=1446 & pcode=1153
+raw.1446<-subset(raw,raw$scrambled_pcode==1446)
+raw.1153<-subset(raw,raw$scrambled_pcode==1153)
+
+#rearrange the network tag
+raw.1446$scrambled_prov<-reflect(as.numeric(names(table(raw.1446$scrambled_prov))),raw.1446$scrambled_prov)
+raw.1446$scrambled_ppn<-reflect(as.numeric(names(table(raw.1446$scrambled_ppn))),raw.1446$scrambled_ppn)
+raw.1446$scrambled_ppn<-raw.1446$scrambled_ppn+max(raw.1446$scrambled_prov)
+
+raw.1153$scrambled_prov<-reflect(as.numeric(names(table(raw.1153$scrambled_prov))),raw.1153$scrambled_prov)
+raw.1153$scrambled_ppn<-reflect(as.numeric(names(table(raw.1153$scrambled_ppn))),raw.1153$scrambled_ppn)
+raw.1153$scrambled_ppn<-raw.1153$scrambled_ppn+max(raw.1153$scrambled_prov)
+
+#turn into adj'mat
+graph.1446<-graph_from_edgelist(as.matrix(raw.1446[,c(1,3)]),directed=FALSE)
+graph.1153<-graph_from_edgelist(as.matrix(raw.1153[,c(1,3)]),directed=FALSE)
+ind.1446<-c(rep(1,max(raw.1446$scrambled_prov)),
+            rep(0,max(raw.1446$scrambled_ppn)-max(raw.1446$scrambled_prov)))
+ind.1153<-c(rep(1,max(raw.1153$scrambled_prov)),
+            rep(0,max(raw.1153$scrambled_ppn)-max(raw.1153$scrambled_prov)))
+adj.mat.1446<-as_adj(graph.1446)
+adj.mat.1153<-as_adj(graph.1153)
+adj.prime.mat.1446<-adj.mat.1446[c(1:sum(ind.1446)),c((sum(ind.1446)+1):length(ind.1446))]
+adj.prime.mat.1153<-adj.mat.1153[c(1:sum(ind.1153)),c((sum(ind.1153)+1):length(ind.1153))]
+
+
+#pcode 1446
+V(graph.1446)$name<-c(1:length(ind.1446))
+V(graph.1446)$type<-ind.1446==0
+proj.graph.1446<-bipartite_projection(graph.1446)$proj1
+lv.1446<-cluster_louvain(proj.graph.1446)
+wc.1446<-cluster_walktrap(proj.graph.1446)
+res.brim.1446<-findModules(as.matrix(adj.prime.mat.1446),spar=FALSE)
+plot(wc.1446,proj.graph.1446,layout=layout.fruchterman.reingold)
+write.csv(data.frame(Label=c(1:sum(ind.1446)),lv.membership=lv.1446$membership),file="nodename_pcode_1446_proj.csv")
+edgelist.1446.proj<-as_edgelist(proj.graph.1446)
+write.csv(data.frame(Source=edgelist.1446.proj[,1]-1,Target=edgelist.1446.proj[,2]-1,Type="Undirected",Weight=E(proj.graph.1446)$weight),file="edge_pcode_1446_proj.csv")
